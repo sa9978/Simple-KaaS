0992c1483f08c32416691339f2c0db885399d82e
Aida Mobli <aidaw.mobli@gmail.com>
2024-07-04 03:49:23 +0330

    healthz is added

--- application_files/app.py
+++ application_files/app.py
@@ -65,6 +65,9 @@ def all_applications():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+@app.route('/healthz', methods=['GET'])
+def health_check():
+    return jsonify({"status": "healthy"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=4040)
